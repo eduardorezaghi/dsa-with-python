25831f2cf0b6a691902a70130a53b4eb245401b2
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2025-01-27 12:56:27 -0300

    Add merge arrays leetcode exercise

--- leetcode_ws/arrays/inplace/merge_arrays.py
+++ leetcode_ws/arrays/inplace/merge_arrays.py
@@ -1,17 +1,24 @@
+# LeetCode: 88. Merge Sorted Array
+# link: https://leetcode.com/problems/merge-sorted-array/
+
 def merge_arrays(nums1: list[int], nums2: list[int], m: int, n: int) -> list[int]:
     ARRAY_SIZE = m + n
     write_index = ARRAY_SIZE - 1
-    L = m - 1
-    R = n - 1
+    L = m - 1 # last index of nums1
+    R = n - 1 # last index of nums2
 
+    # While we are in bounds of both arrays
     while L >= 0 and R >= 0:
+        # If the last element of nums1 is greater than the last element of nums2
         if nums1[L] > nums2[R]:
             nums1[write_index] = nums1[L]
             L -= 1
+        # If the last element of nums2 is greater than the last element of nums1
         else:
             nums1[write_index] = nums2[R]
             R -= 1
 
+        # Move to the next index (bigger to smaller index)
         write_index -= 1
 
     return nums1
